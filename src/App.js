cb63b2e7e4002e2d1a43253bfb771ae346b4329e
YuJie Wu <wuvivian725@gmail.com>
2024-02-28 23:51:01 -0800

    Init the project(setup client, server, and protobuf)

---
+++ src/App.js
@@ -0,0 +1,281 @@
+import "./App.css";
+
+import { faRotateLeft, faRotateRight } from "@fortawesome/free-solid-svg-icons";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { ImageOperation } from "./image_operation_pb";
+import { ImageOperationServiceClient } from "./image_operation_grpc_web_pb";
+import { useState } from "react";
+
+function App() {
+  const [imgSrc, setImgSrc] = useState(null);
+  const [imgBuffer, setImgBuffer] = useState(null);
+  const [flipHorizontal, setFlipHorizontal] = useState(false);
+  const [flipVertical, setFlipVertical] = useState(false);
+  const [thumbnail, setThumbnail] = useState(false);
+  const [grayscale, setGrayscale] = useState(false);
+  const [width, setWidth] = useState(null);
+  const [height, setHeight] = useState(null);
+  const [widthErrorMsg, setWidthErrorMsg] = useState(null);
+  const [heightErrorMsg, setHeightErrorMsg] = useState(null);
+  const [rotateErrorMsg, setRotateErrorMsg] = useState(null);
+  var rotate = [];
+  var rotateDegree = 0;
+  const client = new ImageOperationServiceClient(
+    "http://localhost:8080",
+    null,
+    null
+  );
+
+  const handleImage = (event) => {
+    const file = event.target.files[0];
+    if (!file) {
+      return;
+    }
+
+    const readerArrayBuffer = new FileReader();
+    readerArrayBuffer.onload = (e) => {
+      const buffer = e.target.result;
+      setImgBuffer(buffer);
+    };
+    readerArrayBuffer.readAsArrayBuffer(file);
+
+    const readerDataUrl = new FileReader();
+    readerDataUrl.onload = (e) => {
+      setImgSrc(e.target.result);
+    };
+    readerDataUrl.readAsDataURL(file);
+  };
+
+  const handleFlip = (value) => {
+    if (value === "horizontal") {
+      setFlipHorizontal((prevState) => !prevState);
+    } else {
+      setFlipVertical((prevState) => !prevState);
+    }
+  };
+
+  const handleRotate = (value) => {
+    rotate.push(value);
+  };
+
+  const handleRotateDegree = (e) =>{
+    const reg = /^[-0-9\b]+$/;
+    if(reg.test(e.target.value)){
+      rotateDegree = e.target.value;
+      setRotateErrorMsg(null);
+    }else if(e.target.value==="")
+      setRotateErrorMsg(null);
+    else
+      setRotateErrorMsg('Rotate degree must be number');
+  }
+
+  const handleWidthChange = (e) => {
+    const reg = /^[0-9\b]+$/;
+    if(reg.test(e.target.value)){
+      setWidth(e.target.value);
+      setWidthErrorMsg(null);
+    }else if(e.target.value==="")
+      setWidthErrorMsg(null);
+    else
+      setWidthErrorMsg('Width must be number');
+  };
+
+  const handleHeightChange = (e) => {
+    const reg = /^[0-9\b]+$/;
+    if(reg.test(e.target.value)){
+      setHeight(e.target.value);
+      setHeightErrorMsg(null);
+    }else if(e.target.value==="")
+      setHeightErrorMsg(null);
+    else
+      setHeightErrorMsg('Height must be number');
+  };
+
+  const handleThumbnailChange = () => {
+    setThumbnail((prevState) => !prevState);
+  };
+
+  const handleGrayScaleChange = () => {
+    setGrayscale((prevState) => !prevState);
+  };
+
+  const generateResult = async () => {
+    if(rotateDegree!=0 && rotateDegree!=''){
+      console.log(rotateDegree);
+      rotate.push(rotateDegree);
+    }
+      
+    try {
+      const request = new ImageOperation();
+      request.setImage(new Uint8Array(imgBuffer));
+      request.setRotateList(rotate);
+      request.setWidth(width);
+      request.setHeight(height);
+      request.setGrayscale(grayscale);
+      request.setThumbnails(thumbnail);
+      request.setFliphorizontal(flipHorizontal);
+      request.setFlipvertical(flipVertical);
+      client.getImageOperation(request, {}, function (err, response) {
+        if (err) {
+          console.error("Error:", err);
+        } else {
+          // if there is thumbnail in request, content is response.array[0]
+          let content;
+          let img;
+          let blob;
+          let url;
+          if (request.array[5] === true) {
+            content = response.array[1];
+            blob = new Blob([content[3]], { type: "image/png" });
+            url = URL.createObjectURL(blob);
+            img = document.createElement("img");
+            img.src = url;
+            document.getElementById("result").appendChild(img);
+          } else {
+            content = response.array[0][2];
+            blob = new Blob([content], { type: "image/png" });
+            url = URL.createObjectURL(blob);
+            img = document.createElement("img");
+            img.src = url;
+            document.getElementById("result").appendChild(img);
+          }
+        }
+      });
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <>
+      <label className="w-500 flex">
+        <input
+          className=" hidden"
+          type="file"
+          accept="image/*"
+          onChange={handleImage}
+        />
+        <div className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+          Select Image
+        </div>
+      </label>
+      <div className="grid grid-cols-2 gap-2">
+        <div>
+          <h1>Original Image</h1>
+          {imgSrc != null && (
+            <img src={imgSrc} alt="Uploaded" style={{ maxWidth: "500px" }} />
+          )}
+          <div id="result" className="overflow-y" style={{ maxWidth: "500px" }}>
+            <h1>Processed Images</h1>
+          </div>
+        </div>
+        <div className="grid-flow-col auto-cols-max">
+          <div>
+            <label>Flip Horizontal:</label>
+            <label className="inline-flex items-center cursor-pointer ml-4">
+              <input
+                onChange={() => handleFlip("horizontal")}
+                type="checkbox"
+                value=""
+                className="sr-only peer"
+              />
+              <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+            </label>
+          </div>
+          <div>
+            <label>Flip Vertical:</label>
+            <label className="inline-flex items-center cursor-pointer ml-4">
+              <input
+                onChange={() => handleFlip("vertical")}
+                type="checkbox"
+                value=""
+                className="sr-only peer"
+              />
+              <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+            </label>
+          </div>
+          <div className="mt-5">
+            <label>Rotate:</label>
+            <div className="ml-3 inline-flex">
+              <button
+                onClick={() => handleRotate(-90)}
+                className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l"
+              >
+                RotateLeft <FontAwesomeIcon icon={faRotateLeft} />
+              </button>
+              <button
+                onClick={() => handleRotate(90)}
+                className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r"
+              >
+                RotateRight <FontAwesomeIcon icon={faRotateRight} />
+              </button>
+            </div>
+            <div className="grid grid-flow-row auto-rows-max">
+              <label>Rotate with degree:</label>
+              <input
+                type="text"
+                onChange={handleRotateDegree}
+                className="block w-20 p-3 ps-5 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              />
+              {rotateErrorMsg!=null &&<label className="error-txt">{rotateErrorMsg}</label>}
+            </div>
+          </div>
+          <div className="mt-5">
+            <label>Resize:</label>
+            <div className="grid grid-flow-row auto-rows-max">
+              <label>Width:</label>
+              <input
+                onChange={handleWidthChange}
+                type="text"
+                className="block w-20 p-3 ps-5 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              />
+              {widthErrorMsg!=null &&<label className="error-txt">{widthErrorMsg}</label>}
+              <label>Height:</label>
+              <input
+                onChange={handleHeightChange}
+                type="text"
+                className="block w-20 p-3 ps-5 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              />
+              {heightErrorMsg!=null &&<label className="error-txt">{heightErrorMsg}</label>}
+            </div>
+          </div>
+          <div className="mt-5">
+            <label>Grayscale:</label>
+            <label className="inline-flex items-center cursor-pointer ml-4">
+              <input
+                onChange={handleGrayScaleChange}
+                type="checkbox"
+                value=""
+                className="sr-only peer"
+              />
+              <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+            </label>
+          </div>
+          <div className="mt-5">
+            <label>Thumbnail:</label>
+            <label className="inline-flex items-center cursor-pointer ml-4">
+              <input
+                onChange={handleThumbnailChange}
+                type="checkbox"
+                value=""
+                className="sr-only peer"
+              />
+              <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+            </label>
+          </div>
+          <div>
+            <button
+              onClick={generateResult}
+              className="mt-10 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              GENERATE
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/server.js
@@ -0,0 +1,172 @@
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+const path = require('path'); // Make sure to include the 'path' module
+const sharp = require('sharp');
+// Define the path to your .proto file
+const PROTO_PATH = path.join(__dirname, 'image_operation.proto');
+
+// Load the protobuf definitions
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true,
+});
+
+// Load the package as defined in your .proto file
+const imageProto = grpc.loadPackageDefinition(packageDefinition).image_server;
+
+function processImage(image, rotate, width, height, grayscale, fliphorizontal, flipvertical) {
+    sharp.cache(false);
+
+    let transformer = sharp(image).withMetadata();
+
+    if (rotate) {
+        var degree = 0;
+        rotate.forEach(angle => {
+            // transformer = transformer.rotate(parseInt(angle));
+            degree = degree + parseInt(angle);
+        });
+        transformer = transformer.rotate(parseInt(degree));
+    }
+
+    if (width && height) {
+        transformer = transformer.resize(parseInt(width), parseInt(height));
+    }
+
+    if (grayscale) {
+        transformer = transformer.greyscale();
+    }
+
+    if(fliphorizontal){
+        transformer = transformer.flop();
+    }
+
+    if(flipvertical){
+        transformer = transformer.flip();
+    }
+
+    // Return the transformer promise, which will resolve with the processed image buffer
+    return transformer.toBuffer();
+
+    // sharp.cache(false);
+    // const sharpStream = sharp({
+    //     failOnError: true
+    // });
+    // const transformer = sharp(image)
+    //     .pipe(sharpStream);
+    // if (rotate) {
+    //     for(var i=0; i<rotate.length; i++){
+    //         console.log(rotate[i]);
+    //         transformer.pipe(sharpStream.rotate(parseInt(rotate[i])));
+    //     }
+    // }
+    // if (width && height) {
+    //     transformer.pipe(sharpStream.resize(parseInt(width), parseInt(height)));
+    // }
+    // if (grayscale) {
+    //     transformer.pipe(sharpStream.greyscale());
+    // }
+    // return transformer.pipe(sharpStream);
+}
+
+async function generateThumbnail(image) {
+    // Disable sharp's caching to prevent memory issues in long-running applications.
+    sharp.cache(false);
+
+    try {
+        // Use sharp to resize and convert the image to a thumbnail.
+        const buffer = await sharp(image)
+            .resize(200, 200, {
+                fit: 'contain', // Ensure the image is resized to fit within the specified dimensions.
+                background: { r: 255, g: 255, b: 255, alpha: 1 } // Set the background to white, fully opaque.
+            })
+            .toBuffer(); // Convert the processed image to a Buffer.
+
+        // Convert Buffer to Uint8Array
+        return new Uint8Array(buffer);
+    } catch (err) {
+        console.error('Error generating thumbnail:', err);
+        throw err; // Rethrow the error for further handling.
+    }
+}
+
+const getImageOperation = async (call, callback) => {
+    // Extract the request data
+    const {image, rotate, width, height, grayscale, thumbnails, fliphorizontal, flipvertical} = call.request;
+    // console.log(image)
+    try {
+        // Process the image based on the request parameters
+        // For example, rotate, resize, apply grayscale, etc.
+        // This is where you'd include your image processing logic
+        const processedImage = await processImage(image, rotate, width, height, grayscale, fliphorizontal, flipvertical); // Assuming processImage is synchronous or has been awaited if asynchronous
+
+        let thumbnail = null;
+        if (thumbnails) {
+            thumbnail = await generateThumbnail(processedImage); // Wait for the thumbnail generation
+        }
+        console.log("thumbnail: ",thumbnail);
+
+        // If processing is successful, construct a success response
+        if (thumbnail) {
+            console.log('success with thumbnail')
+            // If a thumbnail was generated, use the success_with_thumbnail response
+            callback(null, {
+
+                success_with_thumbnail: {
+                    code: 200, // Success code
+                    message: "Image processed successfully with thumbnail.",
+                    image: processedImage,
+                    thumbnail_image: thumbnail
+                }
+
+            });
+        } else {
+            console.log('successful without thumbnail')
+            // If no thumbnail was requested or generated, use the simple success response
+            callback(null, {
+                success: {
+                    code: 200,
+                    message: "Image processed successfully.",
+                    image: processedImage
+                }
+            });
+        }
+    } catch (error) {
+        console.log('error')
+        // If there's an error during processing, construct a failure response
+        callback(null, {
+            fail: {
+                code: error.code || 500, // Internal Server Error or a specific error code
+                message: error.message || "Failed to process the image."
+            }
+        });
+    }
+};
+
+
+const main = () => {
+    const server = new grpc.Server();
+
+    // Add the ImageOperationService to the server
+    server.addService(imageProto.ImageOperationService.service, {
+        GetImageOperation: getImageOperation, // Method names should match those defined in your .proto file
+    });
+
+    const host = process.env.HOST || 'localhost';
+    const port = process.env.PORT || '50051';
+    const bindAddress = `${host}:${port}`;
+
+    // Start the server
+    server.bindAsync(bindAddress, grpc.ServerCredentials.createInsecure(), (error, port) => {
+        if (error) {
+            console.error(`Server failed to bind: ${error.message}`);
+            return;
+        }
+        server.start();
+        console.log(`Server started, listening on ${bindAddress}`);
+    });
+};
+
+main();
